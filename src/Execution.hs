db40125caf4536660535dd8759c53d8ec2f88a15
root <root@localhost.localdomain>
2017-03-22 04:51:30 -0400

    get temporary qsub file

--- src/Execution.hs
+++ src/Execution.hs
@@ -6,15 +6,22 @@ import Filesystem.Path.CurrentOS
 import qualified Data.Text as T
 import Turtle
 
-import OptionsParser
 
+import OptionsParser
+import System.Directory
 
 
 execute :: Options -> IO ()
 execute opts = do
+  undefined
+
+getTemplate :: Options -> Shell FilePath
+getTemplate opts = do
     dir <- fmap (fromEither . toText) pwd
     let vs = ("CurrentDirectory", dir) : getArgs opts
-    stdout $ generatePBS (getTemplateFile opts) vs
+    tempF <- getOutputFile opts
+    output tempF (generatePBS (getTemplateFile opts) vs)
+    return tempF
   where
     fromEither (Left a) = a
     fromEither (Right a) = a
@@ -26,14 +33,17 @@ execute opts = do
     getArgs opts =
       [("CMD", T.pack (unwords (command opts)))]
 
--- getTemplaet
+    getOutputFile :: Options -> Shell FilePath
+    getOutputFile opts = do
+      path <- fmap fromString (liftIO (getAppUserDataDirectory ""))
+      using (mktempfile path "jinsub")
 
-generatePBS :: FilePath -> [(Text, Text)] -> Shell Line
-generatePBS temp vars = do
-  let varDefs = map (uncurry getDef) vars
-      pat = defPlaceHolder *> return (T.unlines varDefs)
-  sed pat (input temp)
-  where
-    getDef :: Text -> Text -> Text
-    getDef = format (s % "=\"" %s% "\"")
-    defPlaceHolder = "#DEFS"
+    generatePBS :: FilePath -> [(Text, Text)] -> Shell Line
+    generatePBS temp vars = do
+      let varDefs = map (uncurry getDef) vars
+          pat = defPlaceHolder *> return (T.unlines varDefs)
+      sed pat (input temp)
+      where
+        getDef :: Text -> Text -> Text
+        getDef = format (s % "=\"" %s% "\"")
+        defPlaceHolder = "#DEFS"
