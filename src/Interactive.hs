0f2617cbe4cc4c961ee16f81ec57feaa7bdd600e
root <root@localhost.localdomain>
2017-03-26 02:02:35 -0400

    terminate with eof message

--- src/Interactive.hs
+++ src/Interactive.hs
@@ -2,31 +2,39 @@
 module Interactive where
 
 -- import Control.Concurrent.Async
-import Turtle
+import Turtle hiding (env)
 import System.Process
 import Data.Text as T
-import Prelude hiding (FilePath)
+import Prelude hiding (FilePath, putStrLn)
 import Control.Exception.Base
 import Control.Concurrent
 import Control.Exception
+import Data.Text.IO
 
 interactName :: Text
 interactName = "INTERACT"
 
-jobEndSignal :: Line
+jobEndSignal :: Text
 jobEndSignal = "Jinsub_Interactive_EOF"
 
 interactMode :: Text -> IO ()
 interactMode jobId =
-    catch asyncMonitor (onUserInterupt undefined)
+    catch monitor (onUserInterupt undefined)
   where
-    asyncMonitor = sh $ do
+    monitor = do
       name <- getFileName jobId
       let
-        cmd = format ("tail -n +1 --follow=name --retry " %fp % "2> /dev/null") name
-        watcher = inshell cmd stdin
-      res  <- watcher
-      echo res
+        watcher = createProcess_ "watcher" (tailProcess (T.unpack (format fp name)))
+      (_, Just outH, _, wHandle)  <- watcher
+      iter outH
+      terminateProcess wHandle
+    iter :: Handle -> IO ()
+    iter outH = do
+      l <- hGetLine outH
+      unless (l == jobEndSignal) $
+        do
+          putStrLn l
+          iter outH
     getFileName jobP = fmap (</> getName jobP) home
       where
         getId = fst . T.break (== '.')
@@ -47,3 +55,19 @@ pathToText = fromEither . toText
   where
     fromEither (Left a) = a
     fromEither (Right a) = a
+
+tailProcess :: String -> CreateProcess
+tailProcess str = CreateProcess { cmdspec = ShellCommand ("tail -n +1 --follow=name --retry " ++ str),
+                                  cwd = Nothing,
+                                  env = Nothing,
+                                  std_in = NoStream,
+                                  std_out = CreatePipe,
+                                  std_err = NoStream,
+                                  close_fds = False,
+                                  create_group = False,
+                                  delegate_ctlc = False,
+                                  detach_console = False,
+                                  create_new_console = False,
+                                  new_session = False,
+                                  child_group = Nothing,
+                                  child_user = Nothing }
